79abfb7b192b420001d9ece8486da3d2c4ee1bca
Bo Shang <cnpcshangbo@gmail.com>
2018-12-25 19:18:46 +0800

    to change some lambda to mu

--- obj_fun_apm.m
+++ obj_fun_apm.m
@@ -1,40 +1,30 @@
-function [APM] = obj_fun_apm(x)
+function [obj] = obj_fun_apm(x)
 
-% Objective function based on area phase margin
-% Constants: tau, k, kp, omega_c
-% Inputs: lambda, kd
-% Outputs: obj=area phase margin
-% Demo:
-%  obj_fun_apm([-0.9694 0.6192])
-%  kp = 2.6992;
-lambda=x(1);kd=x(2);
+k_p=x(1);
+k_i=x(2);
+k_d=x(3);
+lambda=x(4);
+mu=x(5);
 
-%% Step one: Preparing necessary data
-% Plant parameters
-global K T1
-K = 1.0263;
-T1 = 0.71;
-tau=T1;k=K;
 
-% Controller design specifications
-global wc phi_m
-% wc = 2.51; %crossover frequency
-% phi_m=83.9;
 
-%% Step two: Calculate area phase margin to build the objective function
-w=wc;
-APM1 = atan2(kd*w^lambda*sin(pi*lambda/2),(1+kd*w^lambda*cos(pi*lambda/2)))...
-    +atan2(1,T1*w);
-w=wc*1.5;
-APM2 = atan2(kd*w^lambda*sin(pi*lambda/2),(1+kd*w^lambda*cos(pi*lambda/2)))...
-    +atan2(1,T1*w);
-w=wc*0.5;
-APM3 = atan2(kd*w^lambda*sin(pi*lambda/2),(1+kd*w^lambda*cos(pi*lambda/2)))...
-    +atan2(1,T1*w);
 
-% APM = abs(phi_m-APM1/pi*180)+abs(phi_m-APM2/pi*180)+abs(phi_m-APM3/pi*180);
-APM = 10^2*abs(phi_m-APM1/pi*180)-APM2/pi*180-APM3/pi*180;
-% to get the min value max, so we use a negative symbol.
 
+
+EE=k_d*omega^lambda*sin(pi*lambda/2);
+FF=1+k_d*omega^lambda*cos(pi*lambda/2);
+phi_m=pi+atan(EE/FF)+atan(1/tau/pi);
+
+AA=k_d * omega_c_minus^lambda * sin(pi*lambda) - k_i*omega_c_minus^(-lambda)*sin(pi*lambda/2);
+BB=k_p + k_i*omega_c_minus^(-lambda)*cos(pi*lambda/2)+k_d*omega_c_minus^lambda*cos(pi*lambda/2);
+phi_m_minus= pi + atan(AA/BB) + atan(1/pi*omega_c_minus);
+
+CC=k_d * omega_c_plus^lambda * sin(pi*lambda) - k_i*omega_c_plus^(-lambda)*sin(pi*lambda/2);
+DD=k_p + k_i*omega_c_plus^(-lambda)*cos(pi*lambda/2)+k_d*omega_c_plus^lambda*cos(pi*lambda/2);
+phi_m_plus= pi + atan(CC/DD) + atan(1/pi*omega_c_plus);
+
+M=100;
+phi_mr=100;
+obj = M * abs(phi_mr-phi_m) - phi_m_minus - phi_m_plus;
 end
 
