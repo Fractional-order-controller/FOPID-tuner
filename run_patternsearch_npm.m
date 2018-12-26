8dfb9cebdcc844dd02a04b2297595f441fee790d
Bo Shang <cnpcshangbo@gmail.com>
2018-12-26 18:36:51 +0800

    Calculated FOPID open-loop transfer function

--- run_patternsearch_npm.m
+++ run_patternsearch_npm.m
@@ -17,3 +17,31 @@ ub=[2,5];
 x = patternsearch(@obj_fun_apm,x0,A,b,Aeq,beq,lb,ub);
 disp('% k_p=x(1); k_i=x(2); k_d=x(3); lambda=x(4); mu=x(5);')
 disp(x);
+
+disp('Plotting Bode plot:');
+k_p=x(1); k_i=x(2); k_d=x(3); lambda=x(4); mu=x(5);
+k = 1.0263;
+tau = 0.71;
+
+disp('Calculating FOPID open-loop transfer function');
+s=fotf('s');
+c_tf=k_p + k_i/s^lambda + k_d*(s^mu);%Test
+p_tf=k/(tau*s+1)/s;
+sys_tf=c_tf*p_tf;
+%
+disp('Calculating IOPID open-loop transfer function');
+ioKp = 1.95;ioKi = 0.369;ioKd = 1.48;
+iokp=ioKp;ioki=ioKi;iokd=ioKd;
+
+ioc_tf=ioKp+ioKi/s+ioKd*s;
+p_tf=k/(tau*s+1)/s;
+iosys_tf=ioc_tf*p_tf;
+%
+h1=figure(2);
+bode(sys_tf);
+grid on
+hold on
+bode(iosys_tf);
+lgd=legend('FOPD','IOPID');
+hold off
+
